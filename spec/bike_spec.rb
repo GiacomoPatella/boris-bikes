bb82356345ea136c2f6d6e64fe0bab3da0471e5d
Giacomo Patella <giacomopatella@gmail.com>
2013-11-25 16:11:57 +0000

    Refactored code and tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,18 +1,24 @@
 # link to the Bike class
 require_relative "../lib/bike"
 
-# We're describing the functionality of a specific class, Bike
+  # We're describing the functionality of a specific class, Bike
 describe Bike do 
-# this is a specific feature (beahviour) that we expect to be present
-it "should not be broken after we create it" do
-  bike = Bike.new # intialise a new object of Bike class
-  expect(bike).not_to be_broken # expect an instance of this class to have a method "broken?" that should return false
-  
+
+  let(:bike) { Bike.new }
+  # this is a specific feature (beahviour) that we expect to be present
+  it "should not be broken after we create it" do
+    expect(bike).not_to be_broken # expect an instance of this class to have a method "broken?" that should return false
   end
 
   it "should be able to break" do
-    bike = Bike.new
     bike.break
     expect(bike).to be_broken
   end
+
+  it "should be able to get fixed" do
+    bike.break
+    bike.fix
+    expect(bike).not_to be_broken
+  end
+
 end
